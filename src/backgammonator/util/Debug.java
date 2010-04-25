810a6e1fefea2be741d29397594f774bf9f71d01
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-25 18:07:10 +0000

    impl for http://code.google.com/p/backgammonator/issues/detail?id=11&colspec=ID%20Status%20Priority%20Milestone%20Owner%20Summary

---
+++ src/backgammonator/util/Debug.java
@@ -0,0 +1,11 @@
+package backgammonator.util;
+
+/**
+ * Used for logging program behavior (warnings, errors, info)
+ * during execution. The debug entry must contain level, message, Throwable
+ * object and module ID.
+ */
+
+public class Debug {
+
+}
