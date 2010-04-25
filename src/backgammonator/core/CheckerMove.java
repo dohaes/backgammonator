4d19d848e20eb0bb39364cb476ebc69e104e59ef
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-25 18:23:02 +0000

    impl http://code.google.com/p/backgammonator/issues/detail?id=8&colspec=ID%20Status%20Priority%20Milestone%20Owner%20Summary

---
+++ src/backgammonator/core/CheckerMove.java
@@ -0,0 +1,41 @@
+package backgammonator.core;
+
+/**
+ * Represents the move of a single checker.
+ */
+public final class CheckerMove {
+	
+	private int from, to;
+
+	/**
+	 * Getter for the position from which the checker is moved
+	 */
+	public int getFrom() {
+		return from;
+	}
+
+	/**
+	 * Setter for the position from which the checker is moved
+	 */
+	public void setFrom(int from) {
+		this.from = from;
+	}
+
+	/**
+	 * Getter for the position to which the checker is moved
+	 */
+	public int getTo() {
+		return to;
+	}
+
+	/**
+	 * Setter for the position to which the checker is moved
+	 */
+	public void setTo(int to) {
+		this.to = to;
+	}
+	
+	
+
+
+}
