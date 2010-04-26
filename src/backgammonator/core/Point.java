ad4ff4d5b7bf07cad9d27ac35c77b9b09c447855
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-26 15:38:28 +0000

    impl for http://code.google.com/p/backgammonator/issues/detail?id=12 and http://code.google.com/p/backgammonator/issues/detail?id=4

--- src/backgammonator/core/Point.java
+++ src/backgammonator/core/Point.java
@@ -1,59 +1,19 @@
 package backgammonator.core;
 
 /**
- * Represents the checkers on a point of the board;
+ * Represents a point of the board includin number
+ * of checkers and color of the checkers.
  */
-public class Point {
-	private int count;
-	private PlayerColor color;
+public interface Point {
+
+  /**
+   * Returns the number of checkers on that point
+   */
+	public int getCount();
 
 	/**
-	 * Creates empty point.
+	 *  Returns the color of the checkers on that point.
 	 */
-	public Point() {
-		this.count = 0;
-		this.color = PlayerColor.WHITE;
-	}
-
-	/**
-	 * Creates point with checkers on it.
-	 * 
-	 * @param count
-	 * @param color
-	 */
-	public Point(int count, PlayerColor color) {
-		if (count < 0 || count > 15) {
-			throw new IllegalArgumentException("Illegal count number: " + count);
-		}
-		this.count = count;
-		this.color = color;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public PlayerColor getColor() {
-		return color;
-	}
-
-	public void decrease() {
-		count--;
-		if (count < 0) {
-			throw new IllegalArgumentException("Illegal count number: " + count);
-		}
-	}
-
-	public boolean increase(PlayerColor color) {
-		if (this.count > 0 && !color.equals(this.color)) {
-			this.count = 1;
-			this.color = color;
-			return true;
-		}
-		count++;
-		if (count > 15) {
-			throw new IllegalArgumentException("Illegal count number: " + count);
-		}
-		return false;
-	}
+	public PlayerColor getColor();
+	
 }
--- src/backgammonator/game/GameManager.java
+++ src/backgammonator/game/GameManager.java
@@ -7,6 +7,7 @@ import backgammonator.core.Logger;
 import backgammonator.core.Player;
 import backgammonator.core.MoveValidator;
 import backgammonator.core.PlayerMove;
+import backgammonator.util.Debug;
 
 /**
  * An instance of this class is created for each game between two players.
@@ -22,9 +23,13 @@ public final class GameManager {
 	
 	private static final long MOVE_TIMEOUT = 1000;
 	
-	private BackgammonBoard board;
+	private BackgammonBoardImpl board;
 	private Dice dice;
 	private Logger logger;
+	
+	private PlayerMove currentMove = null;
+	private Object synch = new Object();
+	private Throwable t = null;
 	
 	/**
 	 * Constructs a game between two AI players.
@@ -32,7 +37,7 @@ public final class GameManager {
 	public GameManager(Player whitePlayer, Player blackPlayer) {
 		this.whitePlayer = whitePlayer;
 		this.blackPlayer = blackPlayer;
-		board = new BackgammonBoard();
+		board = new BackgammonBoardImpl();
 		dice = new Dice();
 		logger = new HTMLLogger();
 	}
@@ -55,6 +60,25 @@ public final class GameManager {
 		
 		logger.endGame(status);
 	}
+	
+	/**
+   * This method starts and navigates the game between the two players.
+   * Always the white player is first.
+   */
+  public void start0() {
+    
+    GameOverStatus status = null;
+    logger.startGame(whitePlayer, blackPlayer);
+    
+    while (true) {
+      status = makeMove0(whitePlayer, blackPlayer);
+      if (status != null) break;
+      status = makeMove0(blackPlayer, whitePlayer);
+      if (status != null) break;
+    }
+    
+    logger.endGame(status);
+  }
 	
 	/**
 	 * Return the end game status if the game is over, on null otherwise
@@ -85,6 +109,121 @@ public final class GameManager {
 		
 		return null;
 	}
+	
+	/**
+   * Return the end game status if the game is over, on null otherwise
+   */
+  private GameOverStatus makeMove0(Player currentPlayer, Player other) {
+    
+    dice.generateNext();
+    mover = new MoveThread(currentPlayer);
+    try {
+      mover.start();
+      waitForMoveDone();
+      
+      if (t != null) {
+        Debug.getInstance().error("Exception thrown while performing move", Debug.CORE_MODULE, t);
+        currentPlayer.gameOver(false);
+        other.gameOver(true);
+        return GameOverStatus.EXCEPTION;
+      }
+      
+      if (!notified) {
+        Debug.getInstance().error("Move timeout", Debug.CORE_MODULE, null);
+        currentPlayer.gameOver(false);
+        other.gameOver(true);
+        stop(mover, true, 100);
+        return GameOverStatus.TIMEDOUT;
+      }
+      
+      if (currentMove == null || !MoveValidator.validateMove(board, currentMove, dice)) {
+        currentPlayer.gameOver(false);
+        other.gameOver(true);
+        return GameOverStatus.INVALID_MOVE;
+      }
+      
+      logger.logMove(currentMove, dice, board.getHits(currentPlayer.getColor()),
+                                    board.getBornOff(currentPlayer.getColor()));
+      board.makeMove(currentMove);
+      
+      if (board.getBornOff(currentPlayer.getColor()) == 15) {
+        currentPlayer.gameOver(true);
+        other.gameOver(false);
+        return GameOverStatus.OK;
+      }
+    } catch (Exception e) {
+      currentPlayer.gameOver(false);
+      other.gameOver(true);
+      return GameOverStatus.EXCEPTION;
+    }
+    
+    return null;
+  }
+  
+  private boolean notified = false;
+  private Thread mover = null;
+  
+  private void waitForMoveDone() {
+    synchronized (synch) {
+      try {
+        synch.wait(MOVE_TIMEOUT);
+      } catch (InterruptedException e) {
+        Debug.getInstance().error("Error waitin for move", Debug.CORE_MODULE, e);
+      }
+    }
+  }
+	
+	final class MoveThread extends Thread {
+	  
+	  private Player currentPlayer;
+	  
+	  MoveThread(Player player) {
+	    currentPlayer = player;
+	    notified = false;
+	    t = null;
+	  }
+	  
+    @Override
+    public void run() {
+      try {
+        currentMove = currentPlayer.getMove(board, dice);
+      } catch (Exception e) {
+        t = e;
+      }
+      synchronized (synch) {
+        notified = true;
+        synch.notify(); 
+      }
+    }
+	}
+	
+	 /**
+   * An utility method to safely stop a thread. Initially it tries to interrupt
+   * the thread. It it is still alive, it stops it. And if stop doesn't help, it
+   * calls destroy.
+   * 
+   * @param thread the thread to stop
+   * @param callStop <code>true</code> to call stop and destroy.
+   * @param joinTime the time to wait for the thread to die.
+   * @return <code>true</code> if the thread is stopped
+   */
+  private static final boolean stop(Thread thread, boolean callStop, int joinTime) {
+    boolean alive = thread.isAlive();
+    if (alive) {
+      try {
+        thread.interrupt();
+      } catch (Throwable t) {}
+      if (callStop && thread.isAlive()) try {
+        thread.join(joinTime);
+        if (thread.isAlive()) thread.stop();
+        if (thread.isAlive()) thread.join(joinTime);
+        if (thread.isAlive()) thread.destroy();
+      } catch (Throwable t) {}
+      alive = thread.isAlive();
+    }
+    return !alive;
+  }
+
 	
 }
 
