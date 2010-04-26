e033ee6d8088c97096e95de3b4a9b5fa7c6cd873
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-26 19:04:35 +0000

    - renamed interface Logger to GameLogger - added GameLoggerFactory

---
+++ src/backgammonator/game/GameLoggerFactory.java
@@ -0,0 +1,46 @@
+package backgammonator.game;
+
+import backgammonator.core.GameLogger;
+
+/**
+ * Represents factory for selecting a desired type of {@link GameLogger} implementation.
+ */
+public class GameLoggerFactory {
+	
+	/**
+	 * If given as argument to {@link #getLogger(int)} specifies that the results
+	 * of the game should be saved in a plain text file.
+	 */
+	public static final int PLAIN = 0;
+	
+	/**
+	 * If given as argument to {@link #getLogger(int)} specifies that the results
+	 * of the game should be saved in an html file.
+	 */
+	public static final int HTML =1;
+	
+	/**
+	 * If given as argument to {@link #getLogger(int)} specifies that the results
+	 * of the game should be printed on the standard output.
+	 */
+	public static final int CONSOLE = 2;
+	
+	/**
+	 * Gets new instance of the selected Logger implementation.
+	 * @param type the type of the logger
+	 * @return new instance of the selected Logger implementation
+	 */
+	public static GameLogger getLogger(int type) {
+		switch (type) {
+		case PLAIN:
+			throw new IllegalArgumentException("Not supported");
+		case CONSOLE:
+			throw new IllegalArgumentException("Not supported");
+		case HTML:
+			return new HTMLLogger();
+		default:
+			throw new IllegalArgumentException("Not supported");
+		}
+	}
+
+}
