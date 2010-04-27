a0450191f641ee29f422c4f05972661be2b954da
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-27 12:35:27 +0000

    added javadoc

--- src/backgammonator/core/GameLogger.java
+++ src/backgammonator/core/GameLogger.java
@@ -8,10 +8,35 @@ package backgammonator.core;
 
 public interface GameLogger {
 
+	/**
+	 * Notifies the logger that a game has started between the specified players.
+	 * The logger should ppepare for logging.
+	 * @param whitePlayer the first player to move
+	 * @param blackPlayer the second player to move 
+	 */
 	void startGame(Player whitePlayer, Player blackPlayer);
 
-	void logMove(PlayerMove move, PlayerColor color, Dice dice, int hit, int bornOff, boolean invalid);
+	/**
+	 * Logs a single move ot the backgammon board.
+	 * 
+	 * @param move the move to log
+	 * @param color the color of the current player to move
+	 * @param dice the numbers on the dice
+	 * @param hits number of hits
+	 * @param bornOffs number of bornOffs
+	 * @param invalid true if the move to log is invalid, ot false otherwise
+	 */
+	void logMove(PlayerMove move, PlayerColor color, Dice dice, int hits, int bornOffs, boolean invalid);
 
+	/**
+	 * Notifies the logger that the game has finished giving the color of the winning pleyer and
+	 * the exist status of the game. The value of the exit status can be {@link GameOverStatus#OK},
+	 * {@link GameOverStatus#INVALID_MOVE}, {@link GameOverStatus#TIMEDOUT}
+	 * or {@link GameOverStatus#EXCEPTION}.
+	 * 
+	 * @param status
+	 * @param winner
+	 */
 	void endGame(GameOverStatus status, PlayerColor winner);
 
 }
