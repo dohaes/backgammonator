4e51e381339be7a112ae1c4a64876b09e04e5b83
georgi.b.andreev <georgi.b.andreev@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-27 17:26:50 +0000

    added test player with invalid move

---
+++ test/backgammonator/test/players/InvalidMovePlayer.java
@@ -0,0 +1,31 @@
+package backgammonator.test.players;
+
+import backgammonator.core.BackgammonBoard;
+import backgammonator.core.CheckerMove;
+import backgammonator.core.Dice;
+import backgammonator.core.Player;
+import backgammonator.core.PlayerMove;
+
+/**
+ * Sample implementation of the {@link Player} interface.
+ * This player returns an invalid move.
+ */
+public class InvalidMovePlayer extends AbstractTestPlayer {
+
+	@Override
+	public PlayerMove getMove(BackgammonBoard board, Dice dice) throws Exception {
+		int die1 = dice.getDie1();
+		int die2 = dice.getDie2();
+		
+		CheckerMove chmove1 = new CheckerMove(2, die1 == 6 ? die1 - 1 : die1 + 1);
+		CheckerMove chmove2 = new CheckerMove(2, die2 == 6 ? die2 - 1 : die2 + 1);
+		
+		return new PlayerMove(new CheckerMove[] {chmove1, chmove2});
+	}
+
+	@Override
+	public String getName() {
+		return "Dummy Player Invalid Move";
+	}
+
+}
