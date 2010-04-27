fee06b6475401f40704029b1937bb4efb52ae079
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-27 01:07:17 +0000

    added test for GameManager

---
+++ trunk/test/backgammonator/test/game/GameManagerTestCase.java
@@ -0,0 +1,93 @@
+package backgammonator.test.game;
+
+import backgammonator.core.GameOverStatus;
+import backgammonator.impl.game.GameManager;
+import backgammonator.test.players.AbstractTestPlayer;
+import backgammonator.test.players.DummyPlayer;
+import backgammonator.test.players.ExceptionPlayer;
+import backgammonator.test.players.NullMovePlayer;
+import backgammonator.test.players.TimedoutMovePlayer;
+import junit.framework.TestCase;
+
+/**
+ * Tests class {@link GameManager}.
+ */
+public class GameManagerTestCase extends TestCase {
+	
+	public void testTimedoutMove() {
+		AbstractTestPlayer timedout = new TimedoutMovePlayer();
+		AbstractTestPlayer normal = new DummyPlayer();
+		
+		GameManager game = new GameManager(timedout, normal, false);
+		GameOverStatus status = null;
+		
+		try {
+			status = game.start();
+		} catch (Throwable t) {
+			fail("Unexpected exception was thrown : " + t);
+		}
+		
+		assertNotNull(status);
+		assertEquals(GameOverStatus.TIMEDOUT, status);
+		assertFalse(timedout.wins());
+		assertTrue(normal.wins());
+	}
+	
+	public void testExceptionInMove() {
+		AbstractTestPlayer exception = new ExceptionPlayer();
+		AbstractTestPlayer normal = new DummyPlayer();
+		
+		GameManager game = new GameManager(exception, normal, false);
+		GameOverStatus status = null;
+		
+		try {
+			status = game.start();
+		} catch (Throwable t) {
+			fail("Unexpected exception was thrown : " + t);
+		}
+		
+		assertNotNull(status);
+		assertEquals(GameOverStatus.EXCEPTION, status);
+		assertFalse(exception.wins());
+		assertTrue(normal.wins());
+	}
+
+	public void testMoveReturnedNull() {
+		AbstractTestPlayer nullmove = new NullMovePlayer();
+		AbstractTestPlayer normal = new DummyPlayer();
+		
+		GameManager game = new GameManager(nullmove, normal, false);
+		GameOverStatus status = null;
+		
+		try {
+			status = game.start();
+		} catch (Throwable t) {
+			fail("Unexpected exception was thrown : " + t);
+		}
+		
+		assertNotNull(status);
+		assertEquals(GameOverStatus.INVALID_MOVE, status);
+		assertFalse(nullmove.wins());
+		assertTrue(normal.wins());
+	}
+	
+	public void testNormal() {
+		AbstractTestPlayer normal1 = new DummyPlayer();
+		AbstractTestPlayer normal2 = new DummyPlayer();
+		
+		GameManager game = new GameManager(normal1, normal2, false);
+		GameOverStatus status = null;
+		
+		try {
+			status = game.start();
+		} catch (Throwable t) {
+			fail("Unexpected exception was thrown : " + t);
+		}
+		
+		assertNotNull(status);
+		assertEquals(GameOverStatus.OK, status);
+		if (normal1.wins()) assertFalse(normal2.wins());
+		else assertTrue(normal2.wins());
+	}
+
+}
