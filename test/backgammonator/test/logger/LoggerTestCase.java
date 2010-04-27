11c13951af98fe2cc613180918b221352517024d
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-04-27 15:14:38 +0000

    added empty testcase for interface Logger

---
+++ test/backgammonator/test/logger/LoggerTestCase.java
@@ -0,0 +1,15 @@
+package backgammonator.test.logger;
+
+import backgammonator.core.GameLogger;
+import junit.framework.TestCase;
+
+/**
+ * Tests classes which implement the {@link GameLogger} interface.
+ */
+public class LoggerTestCase extends TestCase {
+	
+	public void testHTMLLogger() {
+		
+	}
+
+}
