617782d2e660eaef638da968908464afd422d433
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-01 20:45:12 +0000

    - added utility class for tests - added test case for class DiceImpl

--- test/backgammonator/test/game/MoveValidatorTestCase.java
+++ test/backgammonator/test/game/MoveValidatorTestCase.java
@@ -1,48 +1,40 @@
 package backgammonator.test.game;
 
-import java.lang.reflect.Method;
-
 import junit.framework.TestCase;
-import backgammonator.core.Dice;
 import backgammonator.core.PlayerMove;
 import backgammonator.impl.game.BackgammonBoardImpl;
 import backgammonator.impl.game.DiceImpl;
 import backgammonator.impl.game.MoveValidator;
 import backgammonator.test.players.DummyPlayer;
+import backgammonator.test.util.TestUtil;
 
 /**
  * Tests class {@link MoveValidator}
  */
 public class MoveValidatorTestCase extends TestCase {
+	
 	public void testValidMoves() throws Exception {
 		BackgammonBoardImpl board = new BackgammonBoardImpl();
 		DummyPlayer player = new DummyPlayer();
 		DiceImpl dice = new DiceImpl();
 		for (int i = 0; i < 10; i++) {
-			generateNext(dice);
+			TestUtil.generateNext(dice);
 			PlayerMove move = player.getMove(board, dice);
-			assertTrue("Invalid move " + i, makeMove(board, move, dice));
+			assertTrue("Invalid move " + i, TestUtil.makeMove(board, move, dice));
 		}
 	}
 
-	private boolean makeMove(BackgammonBoardImpl board, PlayerMove move,
-			DiceImpl dice) throws Exception {
-		Method m = board.getClass().getDeclaredMethod("makeMove",
-				new Class[] { PlayerMove.class, Dice.class });
-		m.setAccessible(true);
-		Object r = m.invoke(board, move, dice);
-		return ((Boolean) r).booleanValue();
-	}
+	
 
 	public void testInvalidMovesDices() throws Exception {
 		BackgammonBoardImpl board = new BackgammonBoardImpl();
 		DummyPlayer player = new DummyPlayer();
 		DiceImpl dice = new DiceImpl();
 		for (int i = 0; i < 1; i++) {
-			generateNext(dice);
+			TestUtil.generateNext(dice);
 			PlayerMove move = player.getMove(board, dice);
-			generateNext(dice);
-			assertFalse("Invalid move accepted " + i, makeMove(board, move,
+			TestUtil.generateNext(dice);
+			assertFalse("Invalid move accepted " + i, TestUtil.makeMove(board, move,
 					dice));
 		}
 	}
@@ -59,23 +51,5 @@ public class MoveValidatorTestCase extends TestCase {
 	public void testInvalidMovesNoValidMoves() throws Exception {
 	}
 
-	private static Method generateNext = null;
-	static {
-		try {
-			generateNext = DiceImpl.class.getDeclaredMethod("generateNext",
-					(Class[]) null);
-			generateNext.setAccessible(true);
-		} catch (Exception e) {
-		}
-	}
-
-	private static void generateNext(DiceImpl dice) {
-		if (generateNext == null)
-			fail("Method DiceImpl.generateNext not found!");
-		try {
-			generateNext.invoke(dice, (Object[]) null);
-		} catch (Exception e) {
-			fail("Unexpected exception was thrown");
-		}
-	}
+	
 }
