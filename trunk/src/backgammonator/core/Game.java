6fe39832d8884357fc58c3b1487bfedb418955c1
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-01 22:00:15 +0000

    added javadoc

--- trunk/src/backgammonator/core/Game.java
+++ trunk/src/backgammonator/core/Game.java
@@ -9,7 +9,9 @@ public interface Game {
 	/**
 	 * Starts and navigates the game between the two players.
 	 * Always the white player is first.
+	 * 
 	 * @return the status of the game.
+	 * @see GameOverStatus
 	 */
 	GameOverStatus start();
 }
--- trunk/src/backgammonator/impl/game/GameManager.java
+++ trunk/src/backgammonator/impl/game/GameManager.java
@@ -4,10 +4,18 @@ import backgammonator.core.Game;
 import backgammonator.core.Player;
 
 /**
- * Represents ...
+ * This class is used to create a new game between two AI players.
  */
 public class GameManager {
 	
+	/**
+	 * Creates a game between the two AI players given as arguments.
+	 * 
+	 * @param white Player the first player
+	 * @param black Player the second player
+	 * @param logMoves true if the game logger should be enabled and false otherwise
+	 * @return new instance of the {@link Game} interface implementation
+	 */
 	public static Game newGame(Player white, Player black, boolean logMoves) {
 		return new GameImpl(white, black, logMoves);
 	}
