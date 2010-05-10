efeaec0a645d0a4fb07293a4854c2e482fa190bd
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-10 23:09:18 +0000

    moved interface Tournament to core

--- src/backgammonator/impl/tournament/TournamentManager.java
+++ src/backgammonator/impl/tournament/TournamentManager.java
@@ -3,6 +3,7 @@ package backgammonator.impl.tournament;
 import java.util.List;
 
 import backgammonator.core.Player;
+import backgammonator.core.Tournament;
 
 public class TournamentManager {
 	public static Tournament newTournament(List<Player> players) {
