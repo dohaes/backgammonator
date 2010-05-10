e47ce18354f5bdb6275d86dd17d4870a617cd096
andrei.penchev <andrei.penchev@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-10 19:55:10 +0000

    little changes

---
+++ src/backgammonator/impl/game/SourceProcessor.java
@@ -0,0 +1,100 @@
+package backgammonator.impl.game;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Processing the uploaded source files to executables. It should support
+ * processing of java and C/C++ sources. If any errors or warning occur during
+ * the process, the should be shown to the user in an appropriate format.
+ * 
+ */
+public class SourceProcessor {
+
+	/**
+	 * @param filePath
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	public static PlayerImpl processFile(String filePath)
+			throws FileNotFoundException {
+		File file = new File(filePath);
+		if (!file.exists())
+			throw new FileNotFoundException("The file does not exists!");
+		PlayerImpl result = null;
+		Process compileProcess = null;
+		try {
+			compileProcess = Runtime.getRuntime().exec(
+							"cmd.exe /C javac "
+							+ file.getAbsolutePath()
+							+ " -classpath ../../../../lib/backgammonlibrary.jar");
+            // any error message?
+            StreamGobbler errorGobbler = new 
+                StreamGobbler(compileProcess.getErrorStream(), "ERROR");            
+
+            // any output?
+            StreamGobbler outputGobbler = new 
+                StreamGobbler(compileProcess.getInputStream(), "OUTPUT");
+
+            // kick them off
+            errorGobbler.start();
+            outputGobbler.start();
+            
+            //TODO fix file path!
+//			result = new PlayerImpl(
+//					Runtime.getRuntime().exec(
+//							"cmd.exe /C java "
+//							+ file.getAbsolutePath()
+//							+ " -classpath ../../../../lib/backgammonlibrary.jar"));
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			processFile("C:\\Develop\\eclipse\\workspace\\backgammonator\\test\\backgammonator\\test\\players\\AbstractTestPlayer.java");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class StreamGobbler extends Thread
+{
+    InputStream is;
+    String type;
+    StringBuffer output = new StringBuffer();
+
+    StreamGobbler(InputStream is, String type)
+    {
+        this.is = is;
+        this.type = type;
+    }
+
+    public void run()
+    {
+        try
+        {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line=null;
+            while ( (line = br.readLine()) != null)
+                System.out.println(type + ">" + line);
+                output.append(line+"\r\n");
+            } catch (IOException ioe)
+              {
+                ioe.printStackTrace();  
+              }
+    }
+    public String getOutput()
+    {
+        return this.output.toString();
+    }
+}
