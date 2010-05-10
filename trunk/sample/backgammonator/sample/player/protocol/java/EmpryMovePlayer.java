87d9daf6768f61f3f776cb007442d29756668e49
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-10 23:01:49 +0000

    impl for http://code.google.com/p/backgammonator/issues/detail?id=25

---
+++ trunk/sample/backgammonator/sample/player/protocol/java/EmpryMovePlayer.java
@@ -0,0 +1,13 @@
+package backgammonator.sample.player.protocol.java;
+
+/**
+ * Sample implementation of a player using the protocol.
+ * This player returns null for a move.
+ */
+public class EmpryMovePlayer {
+	
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
---
+++ trunk/sample/backgammonator/sample/player/protocol/java/TimedoutMovePlayer.java
@@ -0,0 +1,12 @@
+package backgammonator.sample.player.protocol.java;
+
+/**
+ * Sample implementation of a player using the protocol.
+ * This player is timed out.
+ */
+public class TimedoutMovePlayer {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(2100);
+	}
+}
