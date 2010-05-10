dcc98922664e57e8f6eda6d03ff325f623c68383
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-10 23:01:24 +0000

    added method new game(String, String)

--- trunk/src/backgammonator/impl/game/GameManager.java
+++ trunk/src/backgammonator/impl/game/GameManager.java
@@ -10,6 +10,7 @@ public class GameManager {
 	
 	/**
 	 * Creates a game between the two AI players given as arguments.
+	 * The players are given as Player objects.
 	 * 
 	 * @param white Player the first player
 	 * @param black Player the second player
@@ -18,6 +19,19 @@ public class GameManager {
 	 */
 	public static Game newGame(Player white, Player black, boolean logMoves) {
 		return new GameImpl(white, black, logMoves);
+	}
+	
+	/**
+	 * Creates a game between the two AI players given as arguments.
+	 * The players are given as absolute paths to corresponding the source files.
+	 * 
+	 * @param white String the first player
+	 * @param black String the second player
+	 * @param logMoves true if the game logger should be enabled and false otherwise
+	 * @return new instance of the {@link Game} interface implementation
+	 */
+	public static Game newGame(String white, String black, boolean logMoves) {
+		return null;
 	}
 
 }
