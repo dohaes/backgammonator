cefa912034b39c8ba4e69d42cc9549e96e43db47
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-11 10:24:32 +0000

    implemented the test for java

--- test/backgammonator/test/game/GameWithProtocolTestCase.java
+++ test/backgammonator/test/game/GameWithProtocolTestCase.java
@@ -1,210 +1,238 @@
 package backgammonator.test.game;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import backgammonator.impl.game.GameManager;
 import backgammonator.lib.game.Game;
 import backgammonator.lib.game.GameOverStatus;
-import backgammonator.sample.players.interfacce.AbstractSamplePlayer;
-import backgammonator.sample.players.interfacce.DeadlockInMovePlayer;
-import backgammonator.sample.players.interfacce.EndlessLoopInMovePlayer;
-import backgammonator.sample.players.interfacce.ExceptionPlayer;
-import backgammonator.sample.players.interfacce.InvalidMovePlayer;
-import backgammonator.sample.players.interfacce.NullMovePlayer;
-import backgammonator.sample.players.interfacce.SamplePlayer;
-import backgammonator.sample.players.interfacce.TimedoutMovePlayer;
 import junit.framework.TestCase;
 
 /**
  * Tests class {@link GameImpl}.
  */
-public class GameWithProtocolTestCase extends TestCase { //TODO for protocol impl
-	
+public class GameWithProtocolTestCase extends TestCase {
+
 	private static File testDir = new File("test");
-	
-	@Override
-	protected void setUp() throws Exception {
-		if (!testDir.exists()) testDir.mkdir();
-		super.setUp();
-	}
-	
-	@Override
-	protected void tearDown() throws Exception {
-		if (testDir.exists()) delete(testDir);
-		super.tearDown();
-	}
-	
+	private static String samples = "sample/backgammonator/sample/player/protocol/java";
+
+	private String fileName1;
+	private String fileName2;
+
 	public void testTimedoutMove() {
-		AbstractSamplePlayer timedout = new TimedoutMovePlayer();
-		AbstractSamplePlayer normal = new SamplePlayer();
-		
-		Game game = GameManager.newGame(timedout, normal, false);
-		GameOverStatus status = null;
-		
 		try {
-			status = game.start();
-		} catch (Throwable t) {
-			fail("Unexpected exception was thrown : " + t);
+			copy("TimedoutMovePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.TIMEDOUT, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
 		}
-		
-		assertEquals(normal, game.getWinner());
-		assertNotNull(status);
-		assertEquals(GameOverStatus.TIMEDOUT, status);
-		assertEquals(GameOverStatus.TIMEDOUT, timedout.getStatus());
-		assertEquals(GameOverStatus.TIMEDOUT, normal.getStatus());
-		assertTrue(normal.wins());
-		assertFalse(timedout.wins());
 	}
-	
+
 	public void testExceptionInMove() {
-		AbstractSamplePlayer exception = new ExceptionPlayer();
-		AbstractSamplePlayer normal = new SamplePlayer();
-		
-		Game game = GameManager.newGame(exception, normal, false);
-		GameOverStatus status = null;
-		
 		try {
-			status = game.start();
-		} catch (Throwable t) {
-			fail("Unexpected exception was thrown : " + t);
+			copy("ExceptionPlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.EXCEPTION, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
 		}
-		
-		assertEquals(normal, game.getWinner());
-		assertNotNull(status);
-		assertEquals(GameOverStatus.EXCEPTION, status);
-		assertEquals(GameOverStatus.EXCEPTION, exception.getStatus());
-		assertEquals(GameOverStatus.EXCEPTION, normal.getStatus());
-	  assertTrue(normal.wins());
-    assertFalse(exception.wins());
 	}
 
-	public void testNullReturned() {
-		AbstractSamplePlayer nullmove = new NullMovePlayer();
-		AbstractSamplePlayer normal = new SamplePlayer();
-		
-		Game game = GameManager.newGame(nullmove, normal, false);
-		GameOverStatus status = null;
-		
+	public void testEmptyStringReturned() {
 		try {
-			status = game.start();
-		} catch (Throwable t) {
-			fail("Unexpected exception was thrown : " + t);
+			copy("EmpryMovePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.INVALID_MOVE, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
 		}
-		
-		assertEquals(normal, game.getWinner());
-		assertNotNull(status);
-		assertEquals(GameOverStatus.INVALID_MOVE, status);
-		assertEquals(GameOverStatus.INVALID_MOVE, nullmove.getStatus());
-		assertEquals(GameOverStatus.INVALID_MOVE, normal.getStatus());
-	  assertTrue(normal.wins());
-    assertFalse(nullmove.wins());
 	}
-	
+
 	public void testInvalidMoveReturned() {
-		AbstractSamplePlayer invalid = new InvalidMovePlayer();
-		AbstractSamplePlayer normal = new SamplePlayer();
-		
-		Game game = GameManager.newGame(normal, invalid, false);
-		GameOverStatus status = null;
-		
 		try {
-			status = game.start();
-		} catch (Throwable t) {
-			fail("Unexpected exception was thrown : " + t);
-		}
-		
-		assertEquals(normal, game.getWinner());
-		assertNotNull(status);
-		assertEquals(GameOverStatus.INVALID_MOVE, status);
-		assertEquals(GameOverStatus.INVALID_MOVE, normal.getStatus());
-		assertEquals(GameOverStatus.INVALID_MOVE, invalid.getStatus());
-	  assertTrue(normal.wins());
-    assertFalse(invalid.wins());
-	}
-	
-	public void testEndlessLoopInMoveReturned() {
-    AbstractSamplePlayer endlessloop = new EndlessLoopInMovePlayer();
-    AbstractSamplePlayer normal = new SamplePlayer();
-    
-    Game game = GameManager.newGame(endlessloop, normal, false);
-    GameOverStatus status = null;
-    
-    try {
-      status = game.start();
-    } catch (Throwable t) {
-      fail("Unexpected exception was thrown : " + t);
-    }
-    
-    assertEquals(normal, game.getWinner());
-    assertNotNull(status);
-    assertEquals(GameOverStatus.TIMEDOUT, status);
-    assertEquals(GameOverStatus.TIMEDOUT, normal.getStatus());
-    assertEquals(GameOverStatus.TIMEDOUT, endlessloop.getStatus());
-    assertTrue(normal.wins());
-    assertFalse(endlessloop.wins());
-  }
-	
-	public void testDeadlockInMoveReturned() {
-    AbstractSamplePlayer deadlock = new DeadlockInMovePlayer();
-    AbstractSamplePlayer normal = new SamplePlayer();
-    
-    Game game = GameManager.newGame(deadlock, normal, false);
-    GameOverStatus status = null;
-    
-    try {
-      status = game.start();
-    } catch (Throwable t) {
-      fail("Unexpected exception was thrown : " + t);
-    }
-    
-    assertEquals(normal, game.getWinner());
-    assertNotNull(status);
-    assertEquals(GameOverStatus.TIMEDOUT, status);
-    assertEquals(GameOverStatus.TIMEDOUT, normal.getStatus());
-    assertEquals(GameOverStatus.TIMEDOUT, deadlock.getStatus());
-    assertTrue(normal.wins());
-    assertFalse(deadlock.wins());
-  }
-	
-	public void testNormal() {
-		AbstractSamplePlayer normal1 = new SamplePlayer();
-		AbstractSamplePlayer normal2 = new SamplePlayer();
-		
-		Game game = GameManager.newGame(normal1, normal2, false);
-		GameOverStatus status = null;
-		
-		try {
-			status = game.start();
-		} catch (Throwable t) {
-			fail("Unexpected exception was thrown : " + t);
-		}
-		
-		assertNotNull(status);
-		assertEquals(GameOverStatus.NORMAL, status);
-		GameOverStatus status1 = normal1.getStatus(), status2 = normal2.getStatus();
-		assertEquals(GameOverStatus.NORMAL, status1);
-		assertEquals(GameOverStatus.NORMAL, status2);
-		if (normal1.wins()) {
-		  assertEquals(normal1, game.getWinner());
-		  assertFalse(normal2.wins());
-		} else {
-		  assertEquals(normal2, game.getWinner());
-		  assertTrue(normal2.wins());
+			copy("InvalidMovePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.INVALID_MOVE, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
 		}
 	}
 
-	private boolean delete (File file) {
-		if (file.isFile()) return file.delete();
+	public void testEndlessLoopInMoveReturned() {
+		try {
+			copy("EndlessLoopInMovePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.TIMEDOUT, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
+		}
+	}
+
+	public void testDeadlockInMoveReturned() {
+		try {
+			copy("DeadlockInMovePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.TIMEDOUT, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
+		}
+	}
+
+	public void testNormal() {
+		try {
+			copy("SamplePlayer", "SamplePlayer");
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.NORMAL, status);
+			assertTrue("sampleplayer1".equals(game.getWinner().getName())
+					|| "sampleplayer2".equals(game.getWinner().getName()));
+
+		} finally {
+			cleanup();
+		}
+	}
+
+	private void copy(String name1, String name2) {
+		if (!testDir.exists())
+			testDir.mkdirs();
+		boolean index = name1.equals(name2);
+		String dirName1 = testDir.getAbsolutePath() + File.separatorChar
+				+ name1.toLowerCase() + (index ? "1" : "");
+		String dirName2 = testDir.getAbsolutePath() + File.separatorChar
+				+ name2.toLowerCase() + (index ? "2" : "");
+
+		File dir1 = new File(dirName1);
+		File dir2 = new File(dirName2);
+		dir1.mkdirs();
+		dir2.mkdirs();
+
+		fileName1 = dir1.getAbsolutePath() + File.separatorChar + name1 + ".java";
+		fileName2 = dir2.getAbsolutePath() + File.separatorChar + name2 + ".java";
+
+		String samplesPath = new File(samples.replace('/', File.separatorChar))
+				.getAbsolutePath();
+		try {
+			copyFile(samplesPath + File.separatorChar + name1 + ".java", fileName1);
+			copyFile(samplesPath + File.separatorChar + name2 + ".java", fileName2);
+		} catch (IOException e) {
+			fail("Unexpected exception while cpopying files : " + e.getMessage());
+		}
+
+	}
+
+	private void copyFile(String from, String to) throws IOException {
+		FileInputStream fis = new FileInputStream(new File(from));
+		FileOutputStream fos = new FileOutputStream(new File(to));
+		try {
+
+			byte[] buff = new byte[24];
+			int len;
+
+			while ((len = fis.read(buff, 0, 24)) != 0) {
+				fos.write(buff, 0, len);
+			}
+		} finally {
+			if (fos != null) {
+				fos.flush();
+				fos.close();
+			}
+			if (fis != null)
+				fis.close();
+		}
+
+	}
+
+	private void cleanup() {
+		if (testDir.exists())
+			assertTrue("Could not delete files", delete(testDir));
+	}
+
+	private boolean delete(File file) {
+		if (file.isFile())
+			return file.delete();
 		File[] files = file.listFiles();
-		if (files == null || files.length == 0) return file.delete();
+		if (files == null || files.length == 0)
+			return file.delete();
 		for (int i = 0; i < files.length; i++) {
 			return delete(files[i]);
 		}
 		return true;
-	}
-	
-	private void copy(String name1, String name2) {
-		
-		
 	}
 }
