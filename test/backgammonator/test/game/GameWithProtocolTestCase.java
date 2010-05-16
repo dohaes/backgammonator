dc50ff8488c5abc4c0bf9bf700b775943dafa16b
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-16 20:23:17 +0000

    added tast methods for C/C++ players

--- test/backgammonator/test/game/GameWithProtocolTestCase.java
+++ test/backgammonator/test/game/GameWithProtocolTestCase.java
@@ -20,17 +20,31 @@ import junit.framework.TestCase;
 public class GameWithProtocolTestCase extends TestCase {
 
 	private static File testDir = new File("testGameWithProtocol");
-	private static String samples = "sample/backgammonator/sample/player/protocol/java";
+	private static String samplesJava = "sample/backgammonator/sample/player/protocol/java";
+	private static String samplesC = "sample/backgammonator/sample/player/protocol/c";
 
 	private String fileName1;
 	private String fileName2;
 
 	/**
 	 * Tests in case of time out while getting move from player.
+	 * The test player is implemented in Java.
 	 */
-	public void testTimedoutMove() {
+	public void testTimedoutMoveJava() {
+		testTimedoutMove(true);
+	}
+	
+	/**
+	 * Tests in case of time out while getting move from player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testTimedoutMoveC() { //TODO
+//		testTimedoutMove(false);
+//	}
+	
+	private void testTimedoutMove(boolean java) {
 		try {
-			copy("TimedoutMovePlayer", "SamplePlayer");
+			copy("TimedoutMovePlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -51,10 +65,23 @@ public class GameWithProtocolTestCase extends TestCase {
 
 	/**
 	 * Tests in case of exception while getting move from player.
+	 * The test player is implemented in Java.
 	 */
-	public void testExceptionInMove() {
+	public void testExceptionInMoveJava() {
+		testExceptionInMove(true);
+	}
+	
+	/**
+	 * Tests in case of exception while getting move from player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testExceptionInMoveC() { //TODO
+//		testExceptionInMove(false);
+//	}
+	
+	private void testExceptionInMove(boolean java) {
 		try {
-			copy("ExceptionPlayer", "SamplePlayer");
+			copy("ExceptionPlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -75,10 +102,23 @@ public class GameWithProtocolTestCase extends TestCase {
 	
 	/**
 	 * Tests in case of premature process exit.
+	 * The test player is implemented in Java.
 	 */
-	public void testPrematureProcessExit() {
+	public void testPrematureProcessExitJava() {
+		testPrematureProcessExit(true);
+	}
+	
+	/**
+	 * Tests in case of premature process exit.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testPrematureProcessExitC() { //TODO
+//		testPrematureProcessExit(false);
+//	}
+	
+	private void testPrematureProcessExit(boolean java) {
 		try {
-			copy("PrematureExitPlayer", "SamplePlayer");
+			copy("PrematureExitPlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -99,10 +139,23 @@ public class GameWithProtocolTestCase extends TestCase {
 
 	/**
 	 * Tests in case of empty string returned by player.
+	 * The test player is implemented in Java.
 	 */
-	public void testEmptyStringReturned() {
+	public void testEmptyStringReturnedJava() {
+		testEmptyStringReturned(true);
+	}
+	
+	/**
+	 * Tests in case of empty string returned by player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testEmptyStringReturnedC() { //TODO
+//		testEmptyStringReturned(false);
+//	}
+	
+	private void testEmptyStringReturned(boolean java) {
 		try {
-			copy("EmptyMovePlayer", "SamplePlayer");
+			copy("EmptyMovePlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -123,10 +176,23 @@ public class GameWithProtocolTestCase extends TestCase {
 
 	/**
 	 * Tests in case of invalid move returned by player.
+	 * The test player is implemented in Java.
 	 */
-	public void testInvalidMoveReturned() {
+	public void testInvalidMoveReturnedJava() {
+		testInvalidMoveReturned(true);
+	}
+	
+	/**
+	 * Tests in case of invalid move returned by player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testInvalidMoveReturnedC() { //TODO
+//		testInvalidMoveReturned(false);
+//	}
+	
+	private void testInvalidMoveReturned(boolean java) {
 		try {
-			copy("InvalidMovePlayer", "SamplePlayer");
+			copy("InvalidMovePlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -147,10 +213,23 @@ public class GameWithProtocolTestCase extends TestCase {
 
 	/**
 	 * Tests in case of endless loop while getting move from player.
+	 * The test player is implemented in Java.
 	 */
-	public void testEndlessLoopInMoveReturned() {
+	public void testEndlessLoopInMoveReturnedJava() {
+		testEndlessLoopInMoveReturned(true);
+	}
+	
+	/**
+	 * Tests in case of endless loop while getting move from player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testEndlessLoopInMoveReturnedC() { //TODO
+//		testEndlessLoopInMoveReturned(false);
+//	}
+	
+	private void testEndlessLoopInMoveReturned(boolean java) {
 		try {
-			copy("EndlessLoopInMovePlayer", "SamplePlayer");
+			copy("EndlessLoopInMovePlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -171,34 +250,60 @@ public class GameWithProtocolTestCase extends TestCase {
 
 	/**
 	 * Tests in case of deadlock while getting move from player.
+	 * The test player is implemented in Java.
 	 */
-//	public void testDeadlockInMoveReturned() { //FIXME 
-//		try {
-//			copy("DeadlockInMovePlayer", "SamplePlayer");
-//			Game game = GameManager.newGame(fileName1, fileName2, false);
-//			GameOverStatus status = null;
-//
-//			try {
-//				status = game.start();
-//			} catch (Throwable t) {
-//				fail("Unexpected exception was thrown : " + t);
-//			}
-//
-//			assertNotNull(status);
-//			assertEquals(GameOverStatus.TIMEDOUT, status);
-//			assertEquals("sampleplayer", game.getWinner().getName());
-//
-//		} finally {
-//			cleanup();
-//		}
+//	public void testDeadlockInMoveJava() { 
+//		testDeadlockInMove(true);
 //	}
+	
+	/**
+	 * Tests in case of deadlock while getting move from player.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testDeadlockInMoveC() { 
+//		testDeadlockInMove(false);
+//	}
+	
+	private void testDeadlockInMove(boolean java) { //FIXME 
+		try {
+			copy("DeadlockInMovePlayer", "SamplePlayer", java);
+			Game game = GameManager.newGame(fileName1, fileName2, false);
+			GameOverStatus status = null;
+
+			try {
+				status = game.start();
+			} catch (Throwable t) {
+				fail("Unexpected exception was thrown : " + t);
+			}
+
+			assertNotNull(status);
+			assertEquals(GameOverStatus.TIMEDOUT, status);
+			assertEquals("sampleplayer", game.getWinner().getName());
+
+		} finally {
+			cleanup();
+		}
+	}
 
 	/**
 	 * Tests in case of normal execution of the game.
+	 * The test player is implemented in Java.
 	 */
-	public void testNormal() {
+	public void testNormalJava() {
+		testNormal(true);
+	}
+	
+	/**
+	 * Tests in case of normal execution of the game.
+	 * The test player is implemented in C/C++.
+	 */
+//	public void testNormalC() { //TODO
+//		testNormal(false);
+//	}
+	
+	private void testNormal(boolean java) {
 		try {
-			copy("SamplePlayer", "SamplePlayer");
+			copy("SamplePlayer", "SamplePlayer", java);
 			Game game = GameManager.newGame(fileName1, fileName2, false);
 			GameOverStatus status = null;
 
@@ -218,7 +323,7 @@ public class GameWithProtocolTestCase extends TestCase {
 		}
 	}
 
-	private void copy(String name1, String name2) {
+	private void copy(String name1, String name2, boolean java) {
 		if (!testDir.exists()) testDir.mkdirs();
 		boolean index = name1.equals(name2);
 		String dirName1 = testDir.getAbsolutePath() + File.separatorChar
@@ -236,12 +341,12 @@ public class GameWithProtocolTestCase extends TestCase {
 		fileName2 = dir2.getAbsolutePath() + File.separatorChar + name2
 				+ ".java";
 
-		String samplesPath = new File(samples.replace('/', File.separatorChar))
+		String samplesPath = new File((java ? samplesJava : samplesC).replace('/', File.separatorChar))
 				.getAbsolutePath();
 		try {
-			copyFile(samplesPath + File.separatorChar + name1 + ".java",
+			copyFile(samplesPath + File.separatorChar + name1 + (java ? ".java" : ".c"),
 					fileName1);
-			copyFile(samplesPath + File.separatorChar + name2 + ".java",
+			copyFile(samplesPath + File.separatorChar + name2 + (java ? ".java" : ".c"),
 					fileName2);
 		} catch (IOException e) {
 			fail("Unexpected exception while cpopying files : "
