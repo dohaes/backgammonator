8fff275bbbc5e0f2d56ca46383dd73ec09c78861
andrei.penchev <andrei.penchev@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-05-31 00:44:30 +0000

    adding validateFile implementation

--- src/backgammonator/impl/protocol/SourceProcessor.java
+++ src/backgammonator/impl/protocol/SourceProcessor.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import backgammonator.impl.game.GameManager;
+import backgammonator.lib.game.Game;
+import backgammonator.lib.game.GameOverStatus;
 import backgammonator.lib.game.Player;
 import backgammonator.util.Debug;
 
@@ -59,7 +62,6 @@ public class SourceProcessor {
 				outputGobbler.start();
 
 				int exitCode = compileProcess.waitFor();
-				// TODO notify user for compile error
 				if (exitCode != 0) {
 					Debug.getInstance().error("Compile returned: " + exitCode,
 							Debug.UTILS, null);
@@ -87,6 +89,83 @@ public class SourceProcessor {
 			return result;
 		}
 		return result;
+	}
+
+	/**
+	 * Validate he given file
+	 */
+	public static String validateFile(String filePath) {
+		File file = new File(filePath);
+		boolean isJava;
+		if (file.getName().endsWith(".java")) isJava = true;
+		else if (file.getName().endsWith(".c")) isJava = false;
+		else {
+			return "The file must ends with .java or .c:";
+		}
+
+		Player result = null;
+		Process compileProcess = null;
+		try {
+			if (isJava) {
+				compileProcess = Runtime.getRuntime().exec(
+						"javac " + file.getAbsolutePath());
+
+				int exitCode = compileProcess.waitFor();
+				if (exitCode != 0) {
+					return "Compilation Error!";
+				}
+				File classFile = new File(file.getAbsolutePath().replace(".java",
+						".class"));
+				if (!classFile.exists()) {
+					return "Error with the compilation!";
+				}
+
+				// manage result
+				String mainClass = classFile.getName();
+				mainClass = mainClass.substring(0, mainClass.indexOf("."));
+
+				result = new ProtocolPlayer("java -cp " + classFile.getParent() + " "
+						+ mainClass, classFile.getParentFile().getName());
+			} else {
+				// TODO postponed for now!
+			}
+		} catch (Exception e) {
+			Debug.getInstance().error("Unexpected exception: " + filePath,
+					Debug.UTILS, e);
+			return "Exception:" + e.getMessage();
+		}
+
+		String samplesPath = "sample/backgammonator/sample/player/protocol/java"
+				.replace('/', File.separatorChar);
+		Player player2 = processFile(samplesPath + File.separator
+				+ "SamplePlayer.java");
+
+		Game game = GameManager.newGame(result, player2, true);
+		Object sync = new Object();
+		GameThread gameThead = new GameThread(game, sync);
+
+		for (int i = 0; i < 50; i++) {
+			gameThead.start();
+			try {
+				synchronized (sync) {
+					sync.wait(10000);
+				}
+			} catch (InterruptedException e) {
+				Debug.getInstance().error("Interrupted exception: " + filePath,
+						Debug.UTILS, e);
+			}
+			if (gameThead.getStatus() != GameOverStatus.NORMAL) {
+				return "Problems with the implemented protocol. Our test with"
+						+ "sample player indicated: " + gameThead.getStatus() + " at " + i
+						+ " try";
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		String res = validateFile("sample\\backgammonator\\sample\\player\\protocol\\java\\SamplePlayer.java");
+		System.out.println(res);
 	}
 
 	/**
@@ -142,6 +221,35 @@ class StreamCatcher extends Thread {
 			output.append(line + "\r\n");
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
+		}
+	}
+
+}
+
+class GameThread extends Thread {
+	private Game game;
+	private Object sync;
+	private GameOverStatus status;
+
+	GameThread(Game game, Object sync) {
+		this.game = game;
+		this.sync = sync;
+	}
+
+	/**
+	 * @return game over status
+	 */
+	public GameOverStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @see java.lang.Thread#run()
+	 */
+	public void run() {
+		status = game.start();
+		synchronized (sync) {
+			sync.notify();
 		}
 	}
 }
