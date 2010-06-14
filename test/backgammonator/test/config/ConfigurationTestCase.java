67ae4c1ceb9b3d739c184534e954372c873c6b9b
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-14 16:04:18 +0000

    added test case for Issue 41:[Config] Add support for system configuration information

---
+++ test/backgammonator/test/config/ConfigurationTestCase.java
@@ -0,0 +1,36 @@
+package backgammonator.test.config;
+
+import backgammonator.util.BackgammonatorConfig;
+import junit.framework.TestCase;
+
+/**
+ * Tests configurating the system vie the backgammonator.properties file.
+ */
+public class ConfigurationTestCase extends TestCase {
+
+	/**
+	 * Tests loading of the properties.
+	 */
+	public void testConfigurationLoad() {
+		//test debug configuration
+		assertEquals(false, BackgammonatorConfig.getProperty("backgammonator.debug.enabled", true));
+		assertEquals(false, BackgammonatorConfig.getProperty("backgammonator.debug.onConsole", true));
+		assertEquals(false, BackgammonatorConfig.getProperty("backgammonator.debug.inFile", true));
+		
+		//test game configuration
+        assertEquals(4000, BackgammonatorConfig.getProperty("backgammonator.game.moveTimeout", 1000));
+		assertEquals(20, BackgammonatorConfig.getProperty("backgammonator.game.maxProcesses", 30));
+		assertEquals(1024, BackgammonatorConfig.getProperty("backgammonator.game.maxMemmoryUsage", 200));
+		assertEquals("reports_test", BackgammonatorConfig.getProperty("backgammonator.game.loggerOutputDir"));
+		
+		//test tournament configuration
+		assertEquals("special", BackgammonatorConfig.getProperty("backgammonator.tournament.rate"));
+		assertEquals("reports_test", BackgammonatorConfig.getProperty("backgammonator.tournament.loggerOutputDir"));
+		
+		//test web configuration
+		assertEquals("uploads_test", BackgammonatorConfig.getProperty("backgammonator.web.uploadDir"));
+		assertEquals("repository_test", BackgammonatorConfig.getProperty("backgammonator.web.repositoryDir"));
+		assertEquals(1024, BackgammonatorConfig.getProperty("backgammonator.web.uploadMaxSize", 200));
+	}
+
+}
