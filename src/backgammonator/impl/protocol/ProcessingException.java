d34b45288ca5234d7d64898be12ef4aa9b433bbe
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-15 23:08:11 +0000

    added javadoc

--- src/backgammonator/impl/protocol/ProcessingException.java
+++ src/backgammonator/impl/protocol/ProcessingException.java
@@ -1,29 +1,60 @@
 package backgammonator.impl.protocol;
 
+/**
+ * Custom exception which can be thrown during source processing.
+ */
 public class ProcessingException extends Exception {
-	
-	public static final int FILE_NOT_FOUND =1;
-	public static final int INVALID_EXTENSION =2;
-	public static final int COMPILED_FILE_NOT_FOUND =3;
-	public static final int COMPILATION_ERROR =4;
-	public static final int UNEXPECTED_ERROR =5;
-	
+
+	private static final long serialVersionUID = 6926729429088689254L;
 	private int code;
-	private String message = "Unexpected error";
-	
+
+	/**
+	 * Indicates that the specified file given for processing is not found.
+	 */
+	public static final int FILE_NOT_FOUND = 1;
+
+	/**
+	 * Indicates that the specified file given for processing has invalid
+	 * extension i.e. it is not .java or .c file.
+	 */
+	public static final int INVALID_EXTENSION = 2;
+
+	/**
+	 * Indicates that the output file of the compilation of the specified file
+	 * given for processing is not found.
+	 */
+	public static final int COMPILED_FILE_NOT_FOUND = 3;
+
+	/**
+	 * Indicates that a compilation error occured while processing the specified
+	 * file.
+	 */
+	public static final int COMPILATION_ERROR = 4;
+
+	/**
+	 * Indicates that an unexpected error occured while processing the specified
+	 * file.
+	 */
+	public static final int UNEXPECTED_ERROR = 5;
+
+	/**
+	 * Constructs a new {@link ProcessingException} object.
+	 * 
+	 * @param code the error code.
+	 * @param message the message for the error
+	 */
 	public ProcessingException(int code, String message) {
+		super(message);
 		this.code = code;
-		this.message = message;		
 	}
-	
-	public int getCode(){
+
+	/**
+	 * Returns the error code.
+	 * 
+	 * @return the error code.
+	 */
+	public int getCode() {
 		return code;
 	}
-	
-	@Override
-	public String getMessage() {
-		return message;
-	}
-	
 
 }
