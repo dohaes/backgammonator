aadac10fdd64103aa057891f1d840e514f6968e0
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-16 22:54:30 +0000

    try to load backgammonator.properties as resource on the class path if the file is not found in the project's root directory

--- trunk/src/backgammonator/util/BackgammonatorConfig.java
+++ trunk/src/backgammonator/util/BackgammonatorConfig.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 /**
@@ -11,26 +13,42 @@ import java.util.Properties;
  */
 public final class BackgammonatorConfig {
 
-	private static Properties properties =  new Properties();
+	private static Properties properties = new Properties();
 	private static boolean propertiesInited = false;
+	private static boolean propertiesFileNotFound = false;
 	private static File propertiesFile = new File("backgammonator.properties");
 
 	/**
 	 * Gets the system property associated with the given key.
 	 * 
 	 * @param key the key
-	 * @param alternative the alternative valiue to be returned if not found.
-	 *            Can be <code>null</code>.
+	 * @param alternative the alternative value to be returned if not found. Can
+	 *            be <code>null</code>.
 	 * @return the value of the property or <code>alternative</code> if there is
-	 *         no valiue associated with the given key.
+	 *         no value associated with the given key.
 	 */
 	public static String getProperty(String key, String alternative) {
-		if (!propertiesInited) {
+		if (!propertiesInited && !propertiesFileNotFound) {
 			if (!propertiesFile.exists()) {
+				propertiesFileNotFound = true;
 				Debug.getInstance().warning("Properties file not found",
 						Debug.UTILS, null);
+				// try to find the file as resource from some jar on the class
+				// path
+				URL properies = BackgammonatorConfig.class
+						.getResource("/backgammonator.properties");
+				System.out.println("========== properies : " + properies);
+				if (properies != null) try {
+					properties.load(properies.openStream());
+				} catch (IOException e) {
+					Debug.getInstance().warning(
+							"Error while reading properties file", Debug.UTILS,
+							e);
+				}
+				propertiesInited = true;
 				return alternative;
 			}
+		} else {// properties file exists
 			try {
 				properties.load(new FileInputStream(propertiesFile));
 			} catch (FileNotFoundException e) {
@@ -42,7 +60,8 @@ public final class BackgammonatorConfig {
 			}
 			propertiesInited = true;
 		}
-		return properties.size() == 0 ? alternative : properties.getProperty(key);
+		return properties.size() == 0 ? alternative : properties
+				.getProperty(key);
 	}
 
 	/**
@@ -50,12 +69,12 @@ public final class BackgammonatorConfig {
 	 * 
 	 * @param key the key
 	 * @return the value of the property or <code>null</code> if there is no
-	 *         valiue associated with the given key.
+	 *         value associated with the given key.
 	 */
 	public static String getProperty(String key) {
 		return getProperty(key, null);
 	}
-	
+
 	/**
 	 * @see BackgammonatorConfig#getProperty(String, String)
 	 * @return the value of the property parsed to <code>int</code>
@@ -64,7 +83,7 @@ public final class BackgammonatorConfig {
 		String value = getProperty(key);
 		return value == null ? alternative : Integer.parseInt(value);
 	}
-	
+
 	/**
 	 * @see BackgammonatorConfig#getProperty(String, String)
 	 * @return the value of the property parsed to <code>long</code>
@@ -73,7 +92,7 @@ public final class BackgammonatorConfig {
 		String value = getProperty(key);
 		return value == null ? alternative : Long.parseLong(value);
 	}
-	
+
 	/**
 	 * @see BackgammonatorConfig#getProperty(String, String)
 	 * @return the value of the property parsed to <code>boolean</code>
@@ -82,5 +101,5 @@ public final class BackgammonatorConfig {
 		String value = getProperty(key);
 		return value == null ? alternative : Boolean.parseBoolean(value);
 	}
-	
+
 }
