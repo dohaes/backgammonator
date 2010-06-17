cc15210ad3607365d10b0ae7b328d4d7bb3a338e
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-17 13:30:01 +0000

    added usage message

--- src/backgammonator/impl/game/Main.java
+++ src/backgammonator/impl/game/Main.java
@@ -11,16 +11,20 @@ public class Main {
 
 	/**
 	 * Main method for testing two backgammon players
+	 * 
 	 * @param args the two absolute paths to that source files
 	 */
 	public static void main(String[] args) {
-		if (args.length != 2) throw new IllegalArgumentException(
-				"There must be two arguments!");
+		if (args.length != 2) {
+			System.out.println("Usage : java -jar backgammonatorLibrary.jar "
+					+ "<full_path_to_player1> <full_path_to_player2>");
+			return;
+		}
 
 		Game game = GameManager.newGame(args[0], args[1], true);
 		game.start();
-		//TODO clean up compilation files?
-		//SourceProcessor.cleanUp(args[0]);
-		//SourceProcessor.cleanUp(args[1]);
+		// TODO clean up compilation files?
+		// SourceProcessor.cleanUp(args[0]);
+		// SourceProcessor.cleanUp(args[1]);
 	}
 }
