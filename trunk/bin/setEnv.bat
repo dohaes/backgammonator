b5dede8d3c1b73772545848dbdbe950914106018
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-17 20:54:25 +0000

    impl for Issue 33: [Utils] Add support for processing C/C++ sources

---
+++ trunk/bin/setEnv.bat
@@ -0,0 +1,4 @@
+set Path=%MinGW_HOME%\bin;%Path%
+set C_INCLUDEDE_PATH=%MinGW_HOME%\include
+set CPLUS_INCLUDE_PATH =%MinGW_HOME%\include\c++\3.4.5;%MinGW_HOME%\include\c++\3.4.5\mingw32;%MinGW_HOME%\include\c++\3.4.5\backward;%MinGW_HOME%\include
+set LIBRARY_PATH = %MinGW_HOME%\lib
