0fb7a3feaf9b53453e3541fdc5d8ab7afb5042ec
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-17 23:09:37 +0000

    impl for  Issue 53:[Backgammonator Library] Usage refactoring

--- trunk/src/backgammonator/impl/game/Main.java
+++ trunk/src/backgammonator/impl/game/Main.java
@@ -1,8 +1,10 @@
 package backgammonator.impl.game;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-import backgammonator.impl.protocol.SourceProcessor;
 import backgammonator.lib.game.Game;
 import backgammonator.util.Debug;
 
@@ -12,6 +14,12 @@ import backgammonator.util.Debug;
  * should be given as arguments in an appropriate way.
  */
 public class Main {
+
+	private static File whiteBase = new File("white"); // first player base
+	// directory
+	private static File blackBase = new File("black"); // second player base
+
+	// directory
 
 	/**
 	 * Main method for testing two backgammon players
@@ -50,19 +58,86 @@ public class Main {
 			}
 		}
 
-		Game game = GameManager.newGame(args[0], args[1], true);
+		// check if the given files exist before copying
+		File whitePlayer = new File(args[0]);
+		if (!whitePlayer.exists()) {
+			Debug.getInstance().error("File not found : " + whitePlayer,
+					Debug.DEMO, null);
+		}
+		File blackPlayer = new File(args[1]);
+		if (!blackPlayer.exists()) {
+			Debug.getInstance().error("File not found : " + blackPlayer,
+					Debug.DEMO, null);
+		}
 
+		// initialize the game with the copied sources
+		Game game = null;
+		try {
+			game = GameManager.newGame(copy(whitePlayer, whiteBase), copy(
+					blackPlayer, blackBase), true);
+		} catch (Throwable t) {
+			Debug.getInstance().error("Error while initializing the game",
+					Debug.DEMO, t);
+			return;
+		}
+
+		// execute as many games as defined
 		for (int i = 0; i < runs; i++) {
 			game.start();
 		}
 
-		if (clean) try {
-			SourceProcessor.cleanUp(new File(args[0]).getParent());
-			SourceProcessor.cleanUp(new File(args[2]).getParent());
-		} catch (Throwable t) {
-			Debug.getInstance().error(
-					"Error cleaning compiled files : " + args[0] + " and "
-							+ args[1], Debug.DEMO, t);
+		if (clean) {
+			if (!clean()) {
+				Debug.getInstance().error("Error while cleaning up",
+						Debug.DEMO, null);
+			}
 		}
 	}
+
+	/**
+	 * Copies the source file given as argument to the demo.
+	 * 
+	 * @param source the source file given as argument
+	 * @param toDir the player base directory
+	 * @return the full path to the copied source file
+	 */
+	private static String copy(File source, File toDir) throws IOException {
+		if (!toDir.exists()) toDir.mkdirs();
+		String destFilename = toDir.getAbsolutePath();
+		if (!destFilename.endsWith(File.separator)) destFilename += File.separator;
+		destFilename += source.getName();
+
+		FileInputStream fis = new FileInputStream(source);
+		FileOutputStream fos = new FileOutputStream(new File(destFilename));
+
+		int len = 0;
+		byte[] buff = new byte[24];
+		try {
+			while ((len = fis.read(buff, 0, 24)) != -1) {
+				fos.write(buff, 0, len);
+			}
+		} finally {
+			fis.close();
+			fos.flush();
+			fos.close();
+		}
+
+		return destFilename;
+	}
+
+	private static boolean clean() {
+		return ((whiteBase.exists() && delete(whiteBase))
+				& (blackBase.exists()) && delete(blackBase));
+	}
+
+	private static boolean delete(File file) {
+		if (file.isFile()) return file.delete();
+		File[] files = file.listFiles();
+		if (files == null || files.length == 0) return file.delete();
+		for (int i = 0; i < files.length; i++) {
+			delete(files[i]);
+		}
+		return file.delete();
+	}
+
 }
