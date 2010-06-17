deb761dcd8073f5b451392cf96c79a73c19cf94f
andrei.penchev <andrei.penchev@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-17 21:45:48 +0000

    adding some dummy methods for now

--- db/backgammonator/impl/db/BackgammonatorDB.java
+++ db/backgammonator/impl/db/BackgammonatorDB.java
@@ -2,4 +2,28 @@ package backgammonator.impl.db;
 
 public class BackgammonatorDB {
 
+	/**
+	 * @param user
+	 * @param pass
+	 * @param email
+	 * @param first
+	 * @param last
+	 * 
+	 * @return true if the account was set to db, otherwise - false
+	 */
+	public boolean setAccount(String user, String pass, String email,
+			String first, String last) {
+		return true;
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean isUserExists(String user) {
+		return false;
+	}
+	
+	public boolean login(String user, String pass) {
+		return true;
+	}
 }
