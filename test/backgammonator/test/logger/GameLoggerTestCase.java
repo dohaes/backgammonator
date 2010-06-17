f740c131ba7edfa2b0c0bc66e2a8e4036c921f86
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-17 12:32:28 +0000

    added javadoc

--- test/backgammonator/test/logger/GameLoggerTestCase.java
+++ test/backgammonator/test/logger/GameLoggerTestCase.java
@@ -24,6 +24,9 @@ public class GameLoggerTestCase extends TestCase {
 
 	private GameLogger gameLogger;
 
+	/**
+	 * @see junit.framework.TestCase#setUp()
+	 */
 	@Override
 	protected void setUp() throws Exception {
 		this.gameLogger = GameLoggerFactory.getLogger(GameLoggerFactory.HTML);
@@ -34,6 +37,9 @@ public class GameLoggerTestCase extends TestCase {
 		super.setUp();
 	}
 
+	/**
+	 * Tests logging regular game over.
+	 */
 	public void testRegularEndGame() {
 		this.gameLogger.logMove(new PlayerMove(new CheckerMove[] {
 				new CheckerMove(1, 5), new CheckerMove(1, 2) }), new DiceImpl(
@@ -67,6 +73,9 @@ public class GameLoggerTestCase extends TestCase {
 						+ "<table border=\"1\"><tr><td rowspan=2><b>#</b></td><td rowspan=2><b>Player</b></td><td rowspan=2><b>Die 1</b></td><td rowspan=2><b>Die 2</b></td><td colspan=2><b>Checker Move 1</b></td><td colspan=2><b>Checker Move 2</b></td><td rowspan=2><b>Hit checkers</b></td><td rowspan=2><b>Born off checkers</b></td><td rowspan=2><b>Details</b></td></tr><tr><td><b>Start point</b></td><td><b>Move length</b></td><td><b>Start point</b></td><td><b>Move length</b></td></tr>\n<tr style=\"color:#000000\"><td rowspan=1>1</td><td>black</td><td>5</td><td>2</td><td>1</td><td>5</td><td>1</td><td>2</td><td rowspan=1>0</td><td rowspan=1>0</td><td rowspan=1><a href=\"moves/1.html\" target=\"_blank\">View</a></td></tr>\n<tr style=\"color:#000000\"><td rowspan=2>2</td><td>black</td><td>2</td><td>2</td><td>4</td><td>2</td><td>15</td><td>2</td><td rowspan=2>0</td><td rowspan=2>0</td><td rowspan=2><a href=\"moves/2.html\" target=\"_blank\">View</a></td></tr>\n<tr><td>black</td><td>2</td><td>2</td><td>15</td><td>2</td><td>15</td><td>2</td></tr>\n<tr style=\"color:#0B3B0B\"><td colspan=11>black player wins the game - normal victory</td></tr></table></body></html>\n");
 	}
 
+	/**
+	 * Tests logging invalid move on game over.
+	 */
 	public void testInvalidMoveEndGame() {
 		this.gameLogger.logMove(new PlayerMove(new CheckerMove[] {
 				new CheckerMove(1, 5), new CheckerMove(1, 2) }), new DiceImpl(5, 2), false, new BackgammonBoardImpl());
@@ -98,6 +107,9 @@ public class GameLoggerTestCase extends TestCase {
 						+ "<table border=\"1\"><tr><td rowspan=2><b>#</b></td><td rowspan=2><b>Player</b></td><td rowspan=2><b>Die 1</b></td><td rowspan=2><b>Die 2</b></td><td colspan=2><b>Checker Move 1</b></td><td colspan=2><b>Checker Move 2</b></td><td rowspan=2><b>Hit checkers</b></td><td rowspan=2><b>Born off checkers</b></td><td rowspan=2><b>Details</b></td></tr><tr><td><b>Start point</b></td><td><b>Move length</b></td><td><b>Start point</b></td><td><b>Move length</b></td></tr>\n<tr style=\"color:#000000\"><td rowspan=1>1</td><td>black</td><td>5</td><td>2</td><td>1</td><td>5</td><td>1</td><td>2</td><td rowspan=1>0</td><td rowspan=1>0</td><td rowspan=1><a href=\"moves/1.html\" target=\"_blank\">View</a></td></tr>\n<tr style=\"color:#FF0000\"><td rowspan=2>2</td><td>black</td><td>2</td><td>2</td><td>4</td><td>2</td><td>15</td><td>2</td><td rowspan=2>0</td><td rowspan=2>0</td><td rowspan=2><a href=\"moves/2.html\" target=\"_blank\">View</a></td></tr>\n<tr><td>black</td><td>2</td><td>2</td><td>15</td><td>2</td><td>15</td><td>2</td></tr>\n<tr style=\"color:#FF0000\"><td colspan=11>white player wins the game - invalid move</td></tr></table></body></html>\n");
 	}
 
+	/**
+	 * Tests logging exception on game over.
+	 */
 	public void testExceptionEndGame() {
 		this.gameLogger.logMove(new PlayerMove(new CheckerMove[] {
 				new CheckerMove(1, 5), new CheckerMove(1, 2) }), new DiceImpl(5, 2), false, new BackgammonBoardImpl());
@@ -129,6 +141,9 @@ public class GameLoggerTestCase extends TestCase {
 						+ "<table border=\"1\"><tr><td rowspan=2><b>#</b></td><td rowspan=2><b>Player</b></td><td rowspan=2><b>Die 1</b></td><td rowspan=2><b>Die 2</b></td><td colspan=2><b>Checker Move 1</b></td><td colspan=2><b>Checker Move 2</b></td><td rowspan=2><b>Hit checkers</b></td><td rowspan=2><b>Born off checkers</b></td><td rowspan=2><b>Details</b></td></tr><tr><td><b>Start point</b></td><td><b>Move length</b></td><td><b>Start point</b></td><td><b>Move length</b></td></tr>\n<tr style=\"color:#000000\"><td rowspan=1>1</td><td>black</td><td>5</td><td>2</td><td>1</td><td>5</td><td>1</td><td>2</td><td rowspan=1>0</td><td rowspan=1>0</td><td rowspan=1><a href=\"moves/1.html\" target=\"_blank\">View</a></td></tr>\n<tr style=\"color:#000000\"><td rowspan=2>2</td><td>black</td><td>2</td><td>2</td><td>4</td><td>2</td><td>15</td><td>2</td><td rowspan=2>0</td><td rowspan=2>0</td><td rowspan=2><a href=\"moves/2.html\" target=\"_blank\">View</a></td></tr>\n<tr><td>black</td><td>2</td><td>2</td><td>15</td><td>2</td><td>15</td><td>2</td></tr>\n<tr style=\"color:#FF0000\"><td colspan=11>white player wins the game - exception</td></tr></table></body></html>\n");
 	}
 
+	/**
+	 * @see junit.framework.TestCase#tearDown()
+	 */
 	@Override
 	protected void tearDown() throws Exception {
 		String fileName = BackgammonatorConfig.getProperty("backgammonator.logger.outputDir") + "\\"
