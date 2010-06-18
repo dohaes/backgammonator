20ca2ab61ade0ddc2ee5c3f0f2465c534e32048c
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-18 20:08:31 +0000

    implemented test methods

--- test/backgammonator/test/db/DBManagementTestCase.java
+++ test/backgammonator/test/db/DBManagementTestCase.java
@@ -1,27 +1,32 @@
 package backgammonator.test.db;
 
+import backgammonator.impl.db.AccountsManager;
+import backgammonator.impl.db.DBManager;
+import backgammonator.lib.db.Account;
 import junit.framework.TestCase;
 
 /**
  * Test the API for manipulating the user accounts.
  */
 public class DBManagementTestCase extends TestCase {
-	
+
 	/**
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	@Override
 	protected void setUp() throws Exception {
-		//create the database
+		// create the database
+		DBManager.createDB("BackgammonatorTEST");
 		super.setUp();
 	}
-	
+
 	/**
 	 * @see junit.framework.TestCase#tearDown()
 	 */
 	@Override
 	protected void tearDown() throws Exception {
-		//drop the database
+		// drop the database
+		DBManager.createDB("BackgammonatorTEST");
 		super.tearDown();
 	}
 
@@ -29,17 +34,153 @@ public class DBManagementTestCase extends TestCase {
 	 * Tests creating of user accounts.
 	 */
 	public void testCreatingAccount() {
+		Account testAccount = AccountsManager.getAccount("testcreate");
+		assertNotNull(testAccount);
+		assertFalse(testAccount.exists());
+
+		assertEquals("testcreate", testAccount.getUsername());
+		
+		testAccount.setPassword("password");
+		testAccount.setFirstName("firstName");
+		testAccount.setLastname("lastName");
+		testAccount.setEmail("email");
+
+		testAccount.store();
+
+		testAccount = AccountsManager.getAccount("testcreate");
+		assertNotNull(testAccount);
+		assertTrue(testAccount.exists());
+
+		assertEquals("testcreate", testAccount.getUsername());
+		assertEquals("password", testAccount.getPassword());
+		assertEquals("firstName", testAccount.getFirstName());
+		assertEquals("lastName", testAccount.getLastname());
+		assertEquals("email", testAccount.getEmail());
+
 	}
 
 	/**
 	 * Tests updating of user accounts.
 	 */
 	public void testUpdatingAccount() {
+		Account testAccount = AccountsManager.getAccount("testupdate");
+		assertNotNull(testAccount);
+		assertFalse(testAccount.exists());
+
+		assertEquals("testupdate", testAccount.getUsername());
+		
+		testAccount.setPassword("password");
+		testAccount.setFirstName("firstName");
+		testAccount.setLastname("lastName");
+		testAccount.setEmail("email");
+
+		testAccount.store();
+		
+		testAccount = AccountsManager.getAccount("testupdate");
+		assertNotNull(testAccount);
+		assertTrue(testAccount.exists());
+		
+		testAccount.setPassword("password2");
+		testAccount.setFirstName("firstName2");
+		testAccount.setLastname("lastName2");
+		testAccount.setEmail("email2");
+
+		testAccount.store();
+		
+		testAccount = AccountsManager.getAccount("testupdate");
+		assertNotNull(testAccount);
+		assertTrue(testAccount.exists());
+		
+		assertEquals("testcreate", testAccount.getUsername());
+		assertEquals("password2", testAccount.getPassword());
+		assertEquals("firstName2", testAccount.getFirstName());
+		assertEquals("lastName2", testAccount.getLastname());
+		assertEquals("email2", testAccount.getEmail());
 	}
-	
+
 	/**
 	 * Tests deleting of user accounts.
 	 */
 	public void testDeletingAccount() {
+		Account testAccount = AccountsManager.getAccount("testdelete");
+		assertNotNull(testAccount);
+		assertFalse(testAccount.exists());
+		
+		try {
+			testAccount.delete();
+			fail("Expected IllegalStateException to be thrown");
+		} catch (IllegalStateException ise) {
+			// OK
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
+		
+		testAccount.setPassword("password");
+		testAccount.setFirstName("firstName");
+		testAccount.setLastname("lastName");
+		testAccount.setEmail("email");
+		
+		testAccount.store();
+		testAccount = AccountsManager.getAccount("testupdate");
+		assertNotNull(testAccount);
+		assertTrue(testAccount.exists());
+		
+		try {
+			testAccount.delete();
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
+		
+		assertFalse(testAccount.exists());
+		//check in the db
+		testAccount = AccountsManager.getAccount("testdelete");
+		assertNotNull(testAccount);
+		assertFalse(testAccount.exists());
+		
+	}
+
+	/**
+	 * Tests setting null values.
+	 */
+	public void testNullValues() {
+		Account testAccount = AccountsManager.getAccount("testuser");
+		assertNotNull(testAccount);
+		assertFalse(testAccount.exists());
+
+		try {
+			testAccount.setEmail(null);
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException npe) {
+			// OK
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
+
+		try {
+			testAccount.setPassword(null);
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException npe) {
+			// OK
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
+
+		try {
+			testAccount.setFirstName(null);
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException npe) {
+			// OK
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
+
+		try {
+			testAccount.setLastname(null);
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException npe) {
+			// OK
+		} catch (Throwable t) {
+			fail("Unexpected exception wat thrown: " + t.getMessage());
+		}
 	}
 }
