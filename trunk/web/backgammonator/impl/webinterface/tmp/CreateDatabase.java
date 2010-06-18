0c64ab727ec976ed3fdf68c7873f597202eb1192
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-18 23:10:01 +0000

    fixed to use the DBManager

--- trunk/web/backgammonator/impl/webinterface/tmp/CreateDatabase.java
+++ trunk/web/backgammonator/impl/webinterface/tmp/CreateDatabase.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import backgammonator.impl.db.DBManager;
+
 /**
  * @author georgi.b.andreev
  */
@@ -27,24 +29,7 @@ public final class CreateDatabase extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection theConnection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306", "root", "root");
-			Statement theStatement = theConnection.createStatement();
-
-			try {
-				theStatement.execute("DROP DATABASE Backgammonator");
-			} catch (Exception e) {
-			}
-			theStatement.execute("CREATE DATABASE Backgammonator");
-			theStatement.execute("USE Backgammonator");
-			theStatement.execute("CREATE TABLE Account ( "
-					+ "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-					+ "username varchar(32) NOT NULL UNIQUE, "
-					+ "password varchar(32) NOT NULL, "
-					+ "email varchar(64) NOT NULL, "
-					+ "isadmin bool NOT NULL, " + "first varchar(32), "
-					+ "last varchar(32))");
+			DBManager.createDB();
 //			theStatement
 //					.execute("INSERT INTO Account (username, password, isadmin, first, last) "
 //							+ "VALUES ('georgi', 'andreev', 1, 'georgi', 'andreev')");
@@ -59,19 +44,20 @@ public final class CreateDatabase extends HttpServlet {
 //							+ "VALUES ('elena', 'gramatova', 0, 'elena', 'gramatova')");
 
 			// get current data
-			ResultSet theResult = theStatement
-					.executeQuery("SELECT * FROM Account");
-			while (theResult.next()) {
-				int s = theResult.getMetaData().getColumnCount();
-				for (int i = 1; i <= s; i++) {
-					out.println(theResult.getString(i) + " ");
-				}
-				out.println("<br/>");
-			}
-
-			theResult.close();// Close the result set
-			theStatement.close();// Close statement
-			theConnection.close(); // Close database Connection
+//			
+//			ResultSet theResult = theStatement
+//					.executeQuery("SELECT * FROM Account");
+//			while (theResult.next()) {
+//				int s = theResult.getMetaData().getColumnCount();
+//				for (int i = 1; i <= s; i++) {
+//					out.println(theResult.getString(i) + " ");
+//				}
+//				out.println("<br/>");
+//			}
+//
+//			theResult.close();// Close the result set
+//			theStatement.close();// Close statement
+//			theConnection.close(); // Close database Connection
 		} catch (Exception e) {
 			out.println(e);
 		}
