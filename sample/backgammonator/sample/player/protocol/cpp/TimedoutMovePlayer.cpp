e1d9a2535a2bbb33fba06b624eb5735779a0a1ce
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-21 12:08:39 +0000

    fixed to compile on windows

--- sample/backgammonator/sample/player/protocol/cpp/TimedoutMovePlayer.cpp
+++ sample/backgammonator/sample/player/protocol/cpp/TimedoutMovePlayer.cpp
@@ -1,5 +1,12 @@
-#include<unistd.h>
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#define Sleep(x) usleep((x)*1000)
+#endif
+
 
 int main() {
-	sleep(2000);
+    Sleep(2200);
+	return 0;
 }
