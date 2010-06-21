d1434fbcdb9d7934f2d60a1de9b964b4a5525c29
tsvyatkov <tsvyatkov@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-21 11:15:09 +0000

    Fix in sample players for minGW

--- sample/backgammonator/sample/player/protocol/cpp/InvalidMovePlayer.cpp
+++ sample/backgammonator/sample/player/protocol/cpp/InvalidMovePlayer.cpp
@@ -1,19 +1,17 @@
 #include<iostream>
 
-using namespace std;
-
 int count[25], possesion[25], temp_board[26];
 int hits_mine, hits_opponent, bornoff_mine, bornoff_opponent, die1, die2, status;
 
 void readInput() {
 	for (int i = 1; i < 25; i++) {
-		cin >> count[i];
-		cin >> possesion[i];
+		std::cin >> count[i];
+		std::cin >> possesion[i];
 	}
 
-	cin >> hits_mine >> hits_opponent >> bornoff_mine >> bornoff_opponent;
-	cin >> die1 >> die2;
-	cin >> status;
+	std::cin >> hits_mine >> hits_opponent >> bornoff_mine >> bornoff_opponent;
+	std::cin >> die1 >> die2;
+	std::cin >> status;
 
 }
 
@@ -21,7 +19,7 @@ int main() {
 
 	readInput();
 	while (status == 0) {
-		cout << "24 7 24 1\n";
+		std::cout << "24 7 24 1\n";
 		readInput();
 	}
 	return 0;
