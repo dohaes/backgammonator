190ec9f967e6fcf0be7e9627e63eb95dff2e009c
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-21 14:35:42 +0000

    added sample players for compilation error in java and C++

---
+++ trunk/sample/backgammonator/sample/player/protocol/java/CompilationErrorPlayer.java
@@ -0,0 +1,18 @@
+package backgammonator.sample.player.protocol.java;
+
+/**
+ * Sample implementation of a player using the protocol. This player causes a
+ * compilation error because its source cannot be compiled successfully.
+ */
+public class CompilationErrorPlayer {
+	
+	/**
+	 * The entry point.
+	 * 
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+//		compilation error
+	}
+
+}
