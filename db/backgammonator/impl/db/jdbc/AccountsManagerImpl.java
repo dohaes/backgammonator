d3f3376193c53d24773242e31afb55d286fb8048
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-28 20:50:51 +0000

    implemented method to get all accounts

--- db/backgammonator/impl/db/jdbc/AccountsManagerImpl.java
+++ db/backgammonator/impl/db/jdbc/AccountsManagerImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import backgammonator.lib.db.Account;
@@ -79,8 +80,39 @@ public final class AccountsManagerImpl implements AccountsManager {
 	 */
 	@Override
 	public List<Account> getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = DBManagerImpl.getDBConnection();
+		if (connection == null) {
+			throw new IllegalStateException("Cannot connect to DB!");
+		}
+		Statement statement = null;
+		ResultSet result = null;
+		try {
+			statement = connection.createStatement();
+			result = statement
+					.executeQuery("SELECT * FROM Account");
+
+			List<Account> all = new ArrayList<Account>(10);
+			while (result.next()) {
+				// add each record from the database
+				all.add(new AccountImpl(result.getString("username"), result.getString("password"),
+						result.getString("first"), result.getString("last"), result
+								.getString("email"), result.getBoolean("isadmin")));
+			}
+			return all;
+		} catch (SQLException e) {
+			Debug.getInstance().error("Unexpected Exception was thrown",
+					Debug.DATABASE, e);
+			throw new IllegalStateException(e.getMessage());
+		} finally {
+			try {
+				if (result != null) result.close();
+				if (statement != null) statement.close();
+				connection.close();
+			} catch (SQLException e) {
+				Debug.getInstance().error("Error closing connection",
+						Debug.DATABASE, e);
+			}
+		}
 	}
 
 }
