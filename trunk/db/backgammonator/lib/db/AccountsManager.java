e30a4cf9c708d358342f6f1cd0f0db1f8591bc15
elena.gramatova <elena.gramatova@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-06-28 20:50:51 +0000

    implemented method to get all accounts

--- trunk/db/backgammonator/lib/db/AccountsManager.java
+++ trunk/db/backgammonator/lib/db/AccountsManager.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Accesses user accounts data stored in the database.
  */
 public interface AccountsManager {
-	
+
 	/**
 	 * Gets an {@link Account} object for the specified username. If there is no
 	 * record in the database for the specified username then the
@@ -16,13 +16,12 @@ public interface AccountsManager {
 	 * @throws IllegalStateException if database access error occurs.
 	 */
 	Account getAccount(String username);
-	
+
 	/**
-	 * Gets an {@link Account} object for the specified username. If there is no
-	 * record in the database for the specified username then the
-	 * {@link Account#exists()} method will return false when invoked on the
-	 * returned from this method account object.
+	 * Gets all accounts stored in the database. 
 	 * 
+	 * @return list of all accounts stored in the database. If there are no
+	 *         registered users then an empty list will be returned.
 	 * @throws IllegalStateException if database access error occurs.
 	 */
 	List<Account> getAllAccounts();
