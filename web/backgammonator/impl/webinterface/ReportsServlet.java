2ed96d9d1fd3b03e523dd366cbdda7068f791992
tsvyatkov <tsvyatkov@56160e49-8758-cda3-cca1-5ae903f0e89f>
2010-07-01 22:37:23 +0000

    Added implementation for deleting tournament logs.

--- web/backgammonator/impl/webinterface/ReportsServlet.java
+++ web/backgammonator/impl/webinterface/ReportsServlet.java
@@ -1,9 +1,13 @@
 package backgammonator.impl.webinterface;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
 import backgammonator.util.BackgammonatorConfig;
+import backgammonator.util.Debug;
 
 /**
  * Represents the reports.
@@ -18,6 +23,10 @@ import backgammonator.util.BackgammonatorConfig;
 public final class ReportsServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 7345160614448725636L;
+
+	private final static File REPORT_DIR = new File(
+			BackgammonatorConfig.getProperty("backgammonator.logger.outputDir",
+					"reports"));
 
 	/**
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
@@ -27,8 +36,36 @@ public final class ReportsServlet extends HttpServlet {
 			throws IOException {
 		PrintWriter out = res.getWriter();
 		Util.printHeader(req, out, "Delete Reports", Util.ADMIN);
-		// String tournamentId = (String) request.getAttribute("tid");
-		Util.redirect(out, Util.MANAGE_REPORTS, "Delete successfull.");
+		String tournamentId = req.getParameter("tid");
+		File tournament = new File(REPORT_DIR, tournamentId);
+		if (tournament.exists()) {
+			int len = (int) (tournament.length());
+			FileInputStream fis = new FileInputStream(tournament);
+			byte buf[] = new byte[len];
+			fis.read(buf);
+			fis.close();
+			String resultFileContent = new String(buf, Charset.defaultCharset());
+			Pattern p = Pattern
+					.compile("<a href=\"([^/\"]*)/index.html\" target=\"_blank\">View</a>");
+			Matcher m = p.matcher(resultFileContent);
+			Debug.getInstance().info("Matches:", Debug.WEB_INTERFACE);
+			while (m.find()) {
+				String path = m.group(1);
+				Debug.getInstance().info(path, Debug.WEB_INTERFACE);
+				File f = new File(REPORT_DIR, path);
+				deleteDirectory(f);
+			}
+
+			if (!tournament.delete()) {
+				Util.redirect(out, Util.MANAGE_REPORTS,
+						"The log could not be deleted.");
+			} else {
+				Util.redirect(out, Util.MANAGE_REPORTS, "Delete successfull.");
+			}
+		} else {
+			Util.redirect(out, Util.MANAGE_REPORTS,
+					"The log does not exist and it cannot be deleted.");
+		}
 		Util.printFooter(out);
 	}
 
@@ -42,12 +79,10 @@ public final class ReportsServlet extends HttpServlet {
 	 */
 	public static void printReports(HttpServletRequest req, JspWriter out,
 			boolean manage) throws IOException {
-		File dir = new File(BackgammonatorConfig.getProperty(
-				"backgammonator.logger.outputDir", "reports"));
-		if (!dir.exists()) {
-			dir.mkdirs();
+		if (!REPORT_DIR.exists()) {
+			REPORT_DIR.mkdirs();
 		}
-		String[] tournaments = dir.list(new FilenameFilter() {
+		String[] tournaments = REPORT_DIR.list(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 				return name.endsWith(".html");
 			}
@@ -63,10 +98,24 @@ public final class ReportsServlet extends HttpServlet {
 			out.println("<tr><td> <a href='" + url + "/" + tournaments[i]
 					+ "' target='_blank'> " + tournaments[i] + " </a> </td>");
 			if (manage) {
-				out.println("<td width='20px'>&nbsp;</td><td>&nbsp;</td>");
+				out.println("<td width='20px'>&nbsp;</td><td>"
+						+ "<a href='reports?tid=" + tournaments[i]
+						+ "'> delete </a> </td>");
 			}
 			out.println("</tr>");
 		}
 		out.println("</table>");
+	}
+
+	private boolean deleteDirectory(File path) {
+		File[] files = path.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].isDirectory()) {
+				deleteDirectory(files[i]);
+			} else {
+				files[i].delete();
+			}
+		}
+		return path.delete();
 	}
 }
